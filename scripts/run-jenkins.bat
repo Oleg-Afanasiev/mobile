ceaff25207e8a42abffd0243a9c5a4f5460803ee
Oleg <oleg.kh81@gmail.com>
2019-04-22 02:05:19 +0300

    bat fix

--- scripts/run-jenkins.bat
+++ scripts/run-jenkins.bat
@@ -1,1 +1,1 @@
-start "" java -cp ./target/mobile-2.0/mobile-2.0/lib/*  com.academy.mobile.Application --spring.config.location=file:.//mobile-2.0/mobile-2.0/cfg/application.propertiestarget
+start "" java -cp ../target/mobile-2.0/mobile-2.0/lib/*  com.academy.mobile.Application --spring.config.location=file:../target/mobile-2.0/mobile-2.0/cfg/application.properties
